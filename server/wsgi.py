fb2bdae06a9e2acf83448ef03383063e32c81621
vizalerd <vizalerd@gmail.com>
2021-09-29 20:11:58 +0600

    2 commit

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,10 +1,19 @@
-import sys
-sys.path.insert(0, '/var/www/webroot/ROOT')
+import os,sys
+virtenv = os.path.expanduser('~') + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+   if sys.version.split(' ')[0].split('.')[0] == '3':
+       exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+   else:
+       execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+   pass
 
-activate_this = '/var/www/webroot/ROOT/flask-app/bin/python'
+sys.path.append(os.path.expanduser('~'))
+sys.path.append(os.path.expanduser('~') + '/ROOT/')
+sys.path.append(os.path.expanduser('~') + '/ROOT/ROOT/')
 
-with open(activate_this) as file:
-    exec(file.read(), dict(__file__=activate_this))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ROOT.settings.dev'
 
 from app import app as application
 
