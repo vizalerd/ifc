fcc5c3e6dae88c6e976fd889a2c8a72d6ce3b7e6
vizalerd <vizalerd@gmail.com>
2021-09-30 14:23:04 +0600

    2 commit

--- server/wsgi.py
+++ server/wsgi.py
@@ -2,18 +2,17 @@ import os,sys
 virtenv = os.path.expanduser('~') + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-   if sys.version.split(' ')[0].split('.')[0] == '3':
-       exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
-   else:
-       execfile(virtualenv, dict(__file__=virtualenv))
+    if sys.version.split(' ')[0].split('.')[0] == '3':
+        exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+    else:
+        execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
-   pass
+    pass
 
 sys.path.append(os.path.expanduser('~'))
 sys.path.append(os.path.expanduser('~') + '/ROOT/')
-sys.path.append(os.path.expanduser('~') + '/ROOT/ROOT/')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ROOT.settings.dev'
+os.environ['FLASK_SETTINGS_MODULE'] = 'ROOT.settings'
 
 from app import app as application
 
